7f7e192dfd8f17791787a12062bc09dff93cf1cb
Adnan Ansari <adnan.ans26@gmail.com>
2016-11-14 17:14:18 +0530

    Added reminder and share functionalities to EventDetail activity

--- app/src/main/java/spit/matrix2017/Activities/EventDetails.java
+++ app/src/main/java/spit/matrix2017/Activities/EventDetails.java
@@ -1,14 +1,23 @@
 package spit.matrix2017.Activities;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.Toolbar;
@@ -21,8 +30,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import spit.matrix2017.R;
+
+import static android.R.attr.data;
+import static android.R.attr.end;
 
 public class EventDetails
         extends AppCompatActivity
@@ -30,10 +43,13 @@ public class EventDetails
     private boolean isFavouriteEvent;
     private boolean isReminderSet;
     FloatingActionButton fab;
+    private String event_name;
+    private MenuItem mi_reminder;
+    private long mEventID;
+    private boolean visitedCalendar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -41,61 +57,49 @@ public class EventDetails
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("");
 
+        visitedCalendar = false;
+
         //get intent from eventlist adapter
-        if (getIntent().getStringExtra("name") != null && getSupportActionBar() != null)
-            this.setTitle(getIntent().getStringExtra("name"));
+        if (getIntent().getStringExtra("name") != null && getSupportActionBar() != null) {
+            event_name = getIntent().getStringExtra("name");
+            this.setTitle(event_name);
+        }
         else
             this.setTitle("Some event");
 
-        // TODO: 11/1/2016 Add logic to get selected event from db (FIXED [get it from intent instead])
-        //setDescription(getString(R.string.fake_description));
         setDescription(getIntent().getStringExtra("description"));
-        
         setVenueAndTime(getIntent().getStringExtra("venue"), getIntent().getStringExtra("time"));
-
         setRules();
         setPrizes();
-        
-        //setContacts("Contact Person 1", 9874563210L, "Contact Person 2", 3216549870L);
         setContacts(getIntent().getStringExtra("contact1name"), getIntent().getLongExtra("contact1no", 9999999999l), getIntent().getStringExtra("contact2name"), getIntent().getLongExtra("contact2no", 9999999999l));
 
-        //isFavouriteEvent = false; // TODO: 11/1/2016 according to event set this value (FIXED below)
-        isFavouriteEvent = getIntent().getIntExtra("favorite", 0)==1 ? true : false;
-        // TODO: 11/11/2016 Set the action bar icon based on the value of isFavouriteEvent
-
-        //isReminderSet = false;  // TODO: 11/1/2016 according to event set this value (FIXED below)
-        isReminderSet = getIntent().getIntExtra("reminder", 0)==1 ? true : false;
-        // TODO: 11/11/2016 Set the action bar icon based on the value of isReminderSet
+        isFavouriteEvent = getIntent().getIntExtra("favorite", 0) == 1 ? true : false;
+        isReminderSet = getIntent().getIntExtra("reminder", 0) == 1 ? true : false;
 
         ImageView mainImageView = (ImageView) findViewById(R.id.main_imageView);
         assert mainImageView != null;
-        //mainImageView.setImageResource(R.drawable.virtual_stock_market); // TODO: 11/2/2016 according to event set this value (FIXED below)
         mainImageView.setImageResource(getIntent().getIntExtra("image", R.drawable.virtual_stock_market));
 
         fab = (FloatingActionButton) findViewById(R.id.fab);
         assert fab != null;
-        if(isFavouriteEvent)
-                fab.setImageResource(R.drawable.svg_favorite_white_48px);
-        fab.setOnClickListener(new View.OnClickListener(){
+        if (isFavouriteEvent)
+            fab.setImageResource(R.drawable.svg_favorite_white_48px);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
                 isFavouriteEvent = !isFavouriteEvent;
                 ContentResolver contentResolver = getContentResolver();
                 Uri uri = Uri.parse("content://spit.matrix2017.provider");
                 String selection = "name = ?";
                 String[] selectionArgs = {getIntent().getStringExtra("name")};
                 ContentValues cv = new ContentValues();
-                
-                if(isFavouriteEvent)
-                {
+
+                if (isFavouriteEvent) {
                     cv.put("favorite", 1);
                     contentResolver.update(uri, cv, selection, selectionArgs);
                     Toast.makeText(EventDetails.this, "Added to favorites", Toast.LENGTH_SHORT).show();
                     fab.setImageResource(R.drawable.svg_favorite_white_48px);
-                }
-                else
-                {
+                } else {
                     cv.put("favorite", 0);
                     contentResolver.update(uri, cv, selection, selectionArgs);
                     fab.setImageResource(R.drawable.ic_favorite_border_white_48px);
@@ -113,7 +117,9 @@ public class EventDetails
     @Override
     public boolean onPrepareOptionsMenu(Menu menu)
     {
-        setReminder(menu.findItem(R.id.action_set_reminder), isReminderSet);
+        mi_reminder = menu.findItem(R.id.action_set_reminder);
+        if(isReminderSet)
+            mi_reminder.setIcon(R.drawable.svg_alarm_on_white_48px);
         return super.onPrepareOptionsMenu(menu);
     }
 
@@ -121,10 +127,14 @@ public class EventDetails
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_set_reminder:
-                setReminder(item, !isReminderSet); //used as toggle
+                setReminder();
                 break;
             case R.id.action_share:
-                // TODO: 11/2/2016 create method to share event
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, "Check out the event '"+event_name+"' at Matrix 17! For more details, download the official app from here:\n"+getResources().getString(R.string.playstore_link));
+                intent.setType("text/plain");
+                startActivity(Intent.createChooser(intent, "Share event via"));
                 break;
         }
         return super.onOptionsItemSelected(item);
@@ -162,11 +172,11 @@ public class EventDetails
         assert descriptionTextView != null;
         descriptionTextView.setText(description);
     }
-    
+
     private void setVenueAndTime(String venue, String time) {
         AppCompatTextView venueTimeTextView = (AppCompatTextView) findViewById(R.id.venue_time_textView);
         assert venueTimeTextView != null;
-        venueTimeTextView.setText(venue+" ("+time+")");
+        venueTimeTextView.setText(venue + " (" + time + ")");
     }
 
     private void setRules() {
@@ -272,27 +282,99 @@ public class EventDetails
         saveTwo.setOnClickListener(saveOnClickListener);
     }
 
-    private void setReminder(MenuItem menuItem, boolean isReminderSet) {
-        this.isReminderSet = isReminderSet;
-
-        // TODO: 11/2/2016 logic to update db (FIXED below)
-        ContentResolver contentResolver = getContentResolver();
-        Uri uri = Uri.parse("content://spit.matrix2017.provider");
-        String selection = "name = ?";
-        String[] selectionArgs = {getIntent().getStringExtra("name")};
-        ContentValues cv = new ContentValues();
-
-        if (isReminderSet)
-        {
-            cv.put("reminder", 1);
-            contentResolver.update(uri, cv, selection, selectionArgs);
-            menuItem.setIcon(R.drawable.svg_alarm_on_white_48px);
-        }
+    private void setReminder()
+    {
+        if(ContextCompat.checkSelfPermission(EventDetails.this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CALENDAR}, 1);
         else
         {
-            cv.put("reminder", 0);
-            contentResolver.update(uri, cv, selection, selectionArgs);
-            menuItem.setIcon(R.drawable.svg_alarm_add_white_48px);
+            if (isReminderSet)
+                Toast.makeText(this, "Reminder already set", Toast.LENGTH_SHORT).show();
+            else {
+                final Calendar beginTime = Calendar.getInstance();
+                final Calendar endTime = Calendar.getInstance();
+                beginTime.set(2017, 0, 20, 9, 0);
+                endTime.set(2017, 0, 20, 17, 0);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Remind on?");
+                builder.setItems(new String[]{"Day 1", "Day 2"}, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (i == 1) {
+                            beginTime.set(2017, 0, 21, 9, 0);
+                            endTime.set(2017, 0, 21, 17, 0);
+
+                            goToCalendar(beginTime, endTime);
+                        } else
+                            goToCalendar(beginTime, endTime);
+                    }
+                });
+                builder.show();
+            }
+        }
+    }
+
+    private void goToCalendar(Calendar beginTime, Calendar endTime)
+    {
+        mEventID = getLastEventId(getContentResolver())+1;
+
+        Intent intent = new Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis())
+                .putExtra(CalendarContract.Events._ID, mEventID)
+                .putExtra(CalendarContract.Events.TITLE, event_name)
+                .putExtra(CalendarContract.Events.DESCRIPTION, "Event at Matrix 17")
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, getIntent().getStringExtra("venue")+", S.P.I.T.")
+                .putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY);
+
+        visitedCalendar = true;
+        startActivity(intent);
+    }
+
+    private long getLastEventId(ContentResolver cr)
+    {
+        if(ContextCompat.checkSelfPermission(EventDetails.this, Manifest.permission.READ_CALENDAR) == PackageManager.PERMISSION_GRANTED) {
+            Cursor cursor = cr.query(CalendarContract.Events.CONTENT_URI, new String[]{"MAX(_id) as max_id"}, null, null, "_id");
+            cursor.moveToFirst();
+            long max_val = cursor.getLong(cursor.getColumnIndex("max_id"));
+            return max_val;
+        }
+        return 0;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        if(grantResults[0] != 0)
+            Toast.makeText(EventDetails.this, "Reminder cannot be added without permission to read calendar", Toast.LENGTH_SHORT).show();
+        else
+            setReminder();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (visitedCalendar)
+        {
+            if (getLastEventId(getContentResolver()) == mEventID)
+            {
+                ContentResolver contentResolver = getContentResolver();
+                Uri uri = Uri.parse("content://spit.matrix2017.provider");
+                String selection = "name = ?";
+                String[] selectionArgs = {getIntent().getStringExtra("name")};
+                ContentValues cv = new ContentValues();
+                cv.put("reminder", 1);
+                contentResolver.update(uri, cv, selection, selectionArgs);
+
+                isReminderSet = true;
+                mi_reminder.setIcon(R.drawable.svg_alarm_on_white_48px);
+                Toast.makeText(EventDetails.this, "Successfully added reminder", Toast.LENGTH_SHORT).show();
+            }
+            else
+                Toast.makeText(this, "Reminder not added", Toast.LENGTH_SHORT).show();
         }
     }
 }
