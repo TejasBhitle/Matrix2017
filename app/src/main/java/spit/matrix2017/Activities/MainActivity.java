6908b358905eddd91ab3006ef22a8917285ea4e1
TejasBhitle <bhitletejas@gmail.com>
2016-12-23 09:25:57 +0530

    Drawer Layout Issue and Minor Fixes

--- app/src/main/java/spit/matrix2017/Activities/MainActivity.java
+++ app/src/main/java/spit/matrix2017/Activities/MainActivity.java
@@ -3,6 +3,7 @@ package spit.matrix2017.Activities;
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.internal.NavigationMenuView;
@@ -20,6 +21,9 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
